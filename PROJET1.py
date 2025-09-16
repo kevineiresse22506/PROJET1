1718b2ed82b51a45342c9c758575ae9866b549c5
kevineiresse22506 <kevinenguimfack413@gmail.com>
2025-09-16 16:24:26 -0400

    ajouter les modifications

--- PROJET1.py
+++ PROJET1.py
@@ -1,4 +1,9 @@
 print("hello monde")
 print("one day it will be my turn")
-print("bonjour maman comment tu vas"
+print("bonjour maman comment tu vas")
       
+nb: int
+papa: str
+patate: str
+
+print ( "j'ai faim")
